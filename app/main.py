8f670b76c786655904dc6af5ee149f8150d9d4c8
yukta bande <yuktabande@yuktas-MacBook-Air.local>
2026-06-06 20:45:04 +0530

    fix CORS configuration

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ def create_application() -> FastAPI:
     application.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
-        allow_credentials=True,
+        allow_credentials=False,
         allow_methods=["*"],
         allow_headers=["*"],
     )
